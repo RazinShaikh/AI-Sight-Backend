757ca5af913b373983eef358da2e1caa50e409f6
Razin Shaikh <zy22577@nottingham.edu.cn>
2017-11-10 17:18:42 +0800

    Added detection view.
    Added detection view, which takes a base64 image from JSON and converts it into numpy array.

--- ai_sight_backend/settings.py
+++ ai_sight_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'object_detection.apps.ObjectDetectionConfig',
 ]
 
 MIDDLEWARE = [
--- object_detection/views.py
+++ object_detection/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+
+import base64
+import numpy as np
+from PIL import Image
+import io
+
+class detection(APIView):
+	
+	@csrf_exempt
+	def post(self, request):
+		data = JSONParser().parse(request)
+		img_np = load_base64_image_into_numpy_array(data["img"])
+		return HttpResponse(img_np)
+
+def load_base64_image_into_numpy_array(base64_image):
+	decoded_img = base64.b64decode(base64_image)
+	image_data = Image.open(io.BytesIO(decoded_img))
+	(im_width, im_height) = image_data.size
+	return np.array(image_data.getdata()).reshape(
+		(im_height, im_width, 3)).astype(np.uint8)
