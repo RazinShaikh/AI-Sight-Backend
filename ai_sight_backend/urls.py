342c2f8f55d5dac12777cc3e3941ce6927738eae
Razin Shaikh <zy22577@nottingham.edu.cn>
2017-12-31 16:41:12 +0800

    Add Img2 view Img2 view accepts binary image from client and returns the result in JSON.

--- ai_sight_backend/urls.py
+++ ai_sight_backend/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
-from object_detection.views import Image
+from object_detection.views import Img, Img2
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^detect/', Image.as_view()),
+    url(r'^detect/', Img.as_view()),
+    url(r'^img2/', Img2.as_view()),
 ]
--- object_detection/views.py
+++ object_detection/views.py
@@ -1,9 +1,12 @@
 from django.views.decorators.csrf import csrf_exempt
-
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
+
+import numpy as np
+from PIL import Image
+import io
 
 from .od_core import ai_sight, base64_numpy_conversion
 from ai_sight_backend.settings import BASE_DIR
@@ -25,7 +28,7 @@ PATH_TO_LABELS = os.path.join(BASE_DIR,
                               'mscoco_label_map.pbtxt')
 
 
-class Image(APIView):
+class Img(APIView):
 
     renderer_classes = (JSONRenderer, )
 
@@ -38,6 +41,29 @@ class Image(APIView):
         data = JSONParser().parse(request)
         img_np = base64_numpy_conversion.base64_image_into_numpy_array(
             data["img"])
+        np_img_with_boxes = ai_sight.draw_objects_boxes(img_np)
+        img_b64 = base64_numpy_conversion.numpy_array_to_base64(
+            np_img_with_boxes)
+
+        response = {'img': img_b64}
+
+        return Response(response)
+
+class Img2(APIView):
+
+    renderer_classes = (JSONRenderer, )
+
+    def __init__(self):
+        ai_sight.__init__(PATH_TO_MODEL, PATH_TO_LABELS, 90)
+
+    @csrf_exempt
+    def post(self, request):
+        image_data = Image.open(io.BytesIO(request.body))
+
+        (im_width, im_height) = image_data.size
+        img_np = np.array(image_data.getdata()).reshape(
+            (im_height, im_width, 3)).astype(np.uint8)
+
         np_img_with_boxes = ai_sight.draw_objects_boxes(img_np)
         img_b64 = base64_numpy_conversion.numpy_array_to_base64(
             np_img_with_boxes)
