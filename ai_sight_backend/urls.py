66ad9e99e6ef0f0a8ad0e324e37fc3e35c6e8832
Razin Shaikh <razinshaikh@outlook.com>
2018-04-07 15:59:31 +0800

    Removed Img2 and Img3 from views

--- ai_sight_backend/urls.py
+++ ai_sight_backend/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
-from object_detection.views import Img, Img2, Img3
+from object_detection.views import Img
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^img/', Img.as_view()),
-    url(r'^img2/', Img2.as_view()),
-    url(r'^img3/', Img3.as_view()),
 ]
--- object_detection/od_core/base64_numpy_conversion.py
+++ object_detection/od_core/base64_numpy_conversion.py
@@ -11,12 +11,3 @@ def base64_image_into_numpy_array(base64_image):
     (im_width, im_height) = image_data.size
     return np.array(image_data).reshape(
         (im_height, im_width, 3)).astype(np.uint8)
-
-
-def numpy_array_to_base64(numpy_image):
-    image_data = Image.fromarray(numpy_image)
-    buffered = io.BytesIO()
-    image_data.save(buffered, format="PNG")
-    img_b64 = base64.b64encode(buffered.getvalue())
-
-    return img_b64
